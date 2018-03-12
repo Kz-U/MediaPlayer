a46227f9766adbda19c9a90547b3bfb0073115df
local_user <1193556199@qq.com>
2018-03-12 09:49:44 +0800

    player version2

---
+++ VideoPlayer_6/src/main.cpp
@@ -0,0 +1,37 @@
+﻿
+/**
+ * 叶海辉
+ * QQ群121376426
+ * http://blog.yundiantech.com/
+ */
+
+#include <QApplication>
+#include <QTextCodec>
+
+#include "videoplayer/videoplayer.h"
+#include "CustomTitleWidget/myhelper.h"
+
+#undef main
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+//    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+//    QTextCodec::setCodecForLocale(codec);
+//    QTextCodec::setCodecForCStrings(codec);
+//    QTextCodec::setCodecForTr(codec);
+
+    myHelper::SetUTF8Code();
+//    myHelper::SetStyle("black");//黑色风格
+//    myHelper::SetStyle("blue");//蓝色风格
+    myHelper::SetStyle("gray");//灰色风格
+//    myHelper::SetStyle("navy");//天蓝色风格
+    myHelper::SetChinese();
+
+
+    VideoPlayer w;
+    w.show();
+
+    return a.exec();
+}
+
